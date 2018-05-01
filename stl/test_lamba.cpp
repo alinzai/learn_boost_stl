f2e536b6065ff3cc46e6154305d67aff72b69d5a
csl <alinzai@163.com>
2018-05-01 11:03:55 +0800

    添加捕捉传值的作用域，以及对传值进行修改试验！

--- stl/test_lamba.cpp
+++ stl/test_lamba.cpp
@@ -14,10 +14,31 @@ public:
 private:
   int _x;
 };
+
+
+void test_modify(int & x)
+{
+  x = 4;
+}
+
 int main()
 {
   std::cout << " Hello World!" << std::endl;
   Foo f;
   f.func();
+  int x = 0;
+  int y = 42;
+  auto qqq = [x, &y] {
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+    // test_modify(x);
+    ++y;
+    //++x; // error!
+  };
+
+  x = y = 77;
+  qqq();
+  qqq();
+  std::cout << "final y:" << y << std::endl;
   return 0;
 }
